d8a1ac51b889196687cf62e62ef93f68cee9beb9
francisco huenchuñir <franciscohuenchunir42@gmail.com>
2024-03-18 20:34:28 -0300

    sql and swagger configuration

--- src/main/java/fakeapi/languages/ApiLanguageApplication.java
+++ src/main/java/fakeapi/languages/ApiLanguageApplication.java
@@ -1,6 +1,5 @@
 package fakeapi.languages;
 
-import fakeapi.languages.model.AuthorModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
